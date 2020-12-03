b99532ae56fffbaad8e1a74b32bd9e9cbb72b943
miguel <miguelsodre007@gmail.com>
2020-12-03 15:08:55 -0300

    Applying SSG on dynamic routes

--- src/pages/profile/[id].js
+++ src/pages/profile/[id].js
@@ -1,8 +1,41 @@
-import { useRouter } from 'next/router';
+import Axios from 'axios';
 
-function Profile() {
-  const router = useRouter()
-return <h1>Profile {router.query.id}</h1>
+function Profile({ user = {} }) {
+  return (
+    <div>
+      <p>{user.id}</p>
+      <p>{user.name}</p>
+      <p>{user.username}</p>
+    </div>
+  );
 };
+
+export async function getStaticProps(context) {
+  const response = await Axios.get(
+    'https://jsonplaceholder.typicode.com/users',
+    { params: { id: context.params.id } }
+  );
+  const user = await response.data[0];
+
+  return {
+    props: { user }, // will be passed to the page component as props
+  }
+}
+
+export async function getStaticPaths() {
+  const response = await Axios.get(
+    'https://jsonplaceholder.typicode.com/users',
+  );
+  const user = await response.data;
+
+  const paths = users.map(user => {
+    return { params: { id: String(user.id) } };
+  });
+
+  return {
+    paths,
+    fallback: true, // See the "fallback" section below
+  };
+}
 
 export default Profile;
